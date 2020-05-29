c4d0b504078115e3a115599d54d47550c713c3bf
Milton <milton@domanisystems.com>
2020-05-29 14:55:30 -0400

    add one empty line

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 app.set('view engine','hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
+
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
